084bdf7113dc226555e1c4f370c5a8465d2e8fac
Ryan David Forsyth <ryan@ryans-macbook-air.local>
2019-02-26 21:57:22 -0500

    Update using .xib instead of reusable cell

--- CollectionViewTutorial/ViewController.swift
+++ CollectionViewTutorial/ViewController.swift
@@ -10,7 +10,9 @@ class ViewController: UIViewController, UICollectionViewDelegate, UICollectionVi
     
     @IBOutlet weak var collectionView: UICollectionView!
     
-    let data: [UIImage] = [UIImage(named: "image1")!, UIImage(named: "image2")!, UIImage(named: "image3")!, UIImage(named: "image4")!]
+    let data: [UIImage] = [UIImage(named: "image1")!, UIImage(named: "image2")!, UIImage(named: "image3")!, UIImage(named: "image4")!, UIImage(named: "image1")!, UIImage(named: "image2")!, UIImage(named: "image3")!]
+    
+    let edgeInset: CGFloat = 6
     
     override func viewDidLoad() {
         super.viewDidLoad()
@@ -20,6 +22,7 @@ class ViewController: UIViewController, UICollectionViewDelegate, UICollectionVi
         
         collectionView.dataSource = self
         
+        collectionView.register(UINib(nibName: "PhotoCollectionViewCell", bundle: nil), forCellWithReuseIdentifier: "PhotoCollectionViewCell")
     }
     
     func collectionView(_ collectionView: UICollectionView, numberOfItemsInSection section: Int) -> Int {
@@ -39,14 +42,26 @@ class ViewController: UIViewController, UICollectionViewDelegate, UICollectionVi
         
         let itemsPerRow: CGFloat = 3
         
-        let collectionViewWidth: CGFloat = collectionView.frame.width
+        let padding: CGFloat = 6
+        
+        let collectionViewWidth: CGFloat = collectionView.frame.width - (padding * (itemsPerRow-1)) - (edgeInset*2) // *2 for both sides of the widths
         
         let widthPerItem = collectionViewWidth / itemsPerRow
         
         return CGSize(width: widthPerItem, height: widthPerItem)
         
     }
+    
+    func collectionView(_ collectionView: UICollectionView, layout collectionViewLayout: UICollectionViewLayout, minimumLineSpacingForSectionAt section: Int) -> CGFloat {
+        
+        return CGFloat(6)
+    }
 
+    func collectionView(_ collectionView: UICollectionView, layout collectionViewLayout: UICollectionViewLayout, insetForSectionAt section: Int) -> UIEdgeInsets {
+        
+        return UIEdgeInsets(top: edgeInset, left: edgeInset, bottom: edgeInset, right: edgeInset)
+        
+    }
 
 }
 
